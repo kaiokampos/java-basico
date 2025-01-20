a5a686123502c71f565b70b598c4e97d5d2fc12e
Kaio Kampos <kaiocamposti@gmail.com>
2025-01-19 23:59:40 -0300

    testes

---
+++ data-hora/src/main/java/com/kaiokampos/App.java
@@ -0,0 +1,19 @@
+package com.kaiokampos;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class App {
+    public static void main(String[] args) {
+        LocalDate localDate = LocalDate.now();
+        LocalDateTime dateTime = LocalDateTime.now();
+        Instant instant = Instant.now();
+
+        System.out.println(localDate);
+        System.out.println(dateTime);
+        System.out.println(instant);
+        testes
+
+    }
+}
---
+++ operadores-bitwise/src/main/java/com/kaiokampos/Main.java
@@ -0,0 +1,22 @@
+package com.kaiokampos;
+
+import static java.lang.System.in;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        int n1 = 89;
+        int n2 = 60;
+        System.out.println(n1 & n2);
+        System.out.println(n1 | n2);
+        System.out.println(n1 ^ n2);
+
+
+//        String str = "potato apple lemon";
+//        String[] vector = str.split(" ");
+//
+//        for (int i = 0; i < vector.length; i++){
+//            System.out.println("indice: " +i+ ", valor: "+ vector[i]);
+//        }
+    }
+}
