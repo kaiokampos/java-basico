0dc9bbbccb343bf2f8197b6579b72229cf2984a1
Kaio Kampos <kaiocamposti@gmail.com>
2025-02-14 19:11:52 -0300

    Inserindo dados com retorno da key gerada

--- jdbc/src/main/java/application/Program.java
+++ jdbc/src/main/java/application/Program.java
@@ -17,17 +17,25 @@ public class Program {
             connection = DB.getConnection();
 
             preparedStatement = connection.prepareStatement("INSERT INTO seller (Name, Email, BirthDate, BaseSalary, DepartmentId)" +
-                    "VALUES (?,?,?,?,?)");
+                    "VALUES (?,?,?,?,?)", preparedStatement.RETURN_GENERATED_KEYS);
 
-            preparedStatement.setString(1,"Kaio");
-            preparedStatement.setString(2,"Kaio@kaio.com.br");
+            preparedStatement.setString(1,"Robson");
+            preparedStatement.setString(2,"robson@robson.com.br");
             preparedStatement.setDate(3,new java.sql.Date(simpleDateFormat.parse("22/10/2013").getTime()));
             preparedStatement.setDouble(4, 5000.0);
             preparedStatement.setInt(5, 4);
 
             int rowsAffected = preparedStatement.executeUpdate();
 
-            System.out.println("Done! Rows affected "+ rowsAffected);
+            if (rowsAffected > 0){
+                ResultSet resultSet = preparedStatement.getGeneratedKeys();
+                while (resultSet.next()){
+                    int id = resultSet.getInt(1);
+                    System.out.println("Done Id = " + id);
+                }
+            }else {
+                System.out.println("No rown affected!");
+            }
 
         } catch (SQLException e) {
             e.printStackTrace();
