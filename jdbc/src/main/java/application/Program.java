6fe596a99df86c66d322aacad53d49be4569a778
Kaio Kampos <kaiocamposti@gmail.com>
2025-02-14 19:04:58 -0300

    Inserindo dados

--- jdbc/src/main/java/application/Program.java
+++ jdbc/src/main/java/application/Program.java
@@ -2,32 +2,43 @@ package application;
 
 import db.DB;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 public class Program {
     public static void main(String[] args) {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
+
         Connection connection = null;
-        Statement statement = null;
-        ResultSet resultSet = null;
+        PreparedStatement preparedStatement = null;
 
         try {
             connection = DB.getConnection();
-            statement = connection.createStatement();
-            resultSet = statement.executeQuery("SELECT * FROM department");
 
-            while (resultSet.next()){
-                System.out.println(resultSet.getInt("Id") + ", " + resultSet.getString("Name"));
-            }
+            preparedStatement = connection.prepareStatement("INSERT INTO seller (Name, Email, BirthDate, BaseSalary, DepartmentId)" +
+                    "VALUES (?,?,?,?,?)");
 
-        }catch (SQLException e) {
+            preparedStatement.setString(1,"Kaio");
+            preparedStatement.setString(2,"Kaio@kaio.com.br");
+            preparedStatement.setDate(3,new java.sql.Date(simpleDateFormat.parse("22/10/2013").getTime()));
+            preparedStatement.setDouble(4, 5000.0);
+            preparedStatement.setInt(5, 4);
+
+            int rowsAffected = preparedStatement.executeUpdate();
+
+            System.out.println("Done! Rows affected "+ rowsAffected);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }catch (ParseException e){
             e.printStackTrace();
         }finally {
-            DB.closeResultSet(resultSet);
-            DB.closeStatement(statement);
+            DB.closeStatement(preparedStatement);
             DB.closeConnection();
         }
+
+
+
     }
 }
