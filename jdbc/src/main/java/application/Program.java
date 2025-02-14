14184c9d5f2e9902f7d12ac030faf76da2a2eb13
Kaio Kampos <kaiocamposti@gmail.com>
2025-02-14 19:34:10 -0300

    Atualizando dados

--- jdbc/src/main/java/application/Program.java
+++ jdbc/src/main/java/application/Program.java
@@ -8,45 +8,27 @@ import java.text.SimpleDateFormat;
 
 public class Program {
     public static void main(String[] args) {
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
-
         Connection connection = null;
         PreparedStatement preparedStatement = null;
 
         try {
             connection = DB.getConnection();
 
-            preparedStatement = connection.prepareStatement("INSERT INTO seller (Name, Email, BirthDate, BaseSalary, DepartmentId)" +
-                    "VALUES (?,?,?,?,?)", preparedStatement.RETURN_GENERATED_KEYS);
+            preparedStatement = connection.prepareStatement("UPDATE seller SET BaseSalary = BaseSalary + ? WHERE (DepartmentId = ?)");
 
-            preparedStatement.setString(1,"Robson");
-            preparedStatement.setString(2,"robson@robson.com.br");
-            preparedStatement.setDate(3,new java.sql.Date(simpleDateFormat.parse("22/10/2013").getTime()));
-            preparedStatement.setDouble(4, 5000.0);
-            preparedStatement.setInt(5, 4);
+            preparedStatement.setDouble(1, 200);
+            preparedStatement.setInt(2, 2);
 
             int rowsAffected = preparedStatement.executeUpdate();
 
-            if (rowsAffected > 0){
-                ResultSet resultSet = preparedStatement.getGeneratedKeys();
-                while (resultSet.next()){
-                    int id = resultSet.getInt(1);
-                    System.out.println("Done Id = " + id);
-                }
-            }else {
-                System.out.println("No rown affected!");
-            }
+            System.out.println("Done! Rows affected "+ rowsAffected);
 
         } catch (SQLException e) {
-            e.printStackTrace();
-        }catch (ParseException e){
             e.printStackTrace();
         }finally {
             DB.closeStatement(preparedStatement);
             DB.closeConnection();
         }
-
-
 
     }
 }
