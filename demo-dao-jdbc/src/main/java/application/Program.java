c18bdad17aad1dbcdd83e5c29906b32c3f5235c4
Kaio Kampos <kaiocamposti@gmail.com>
2025-02-18 11:55:35 -0300

    Seller entity class

--- demo-dao-jdbc/src/main/java/application/Program.java
+++ demo-dao-jdbc/src/main/java/application/Program.java
@@ -1,10 +1,16 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
         Department department = new Department(1, "books");
-        System.out.println(department);
+
+        Seller seller = new Seller(1, "bob", "bob@gmail.com", new Date(), 3000.00, department);
+
+        System.out.println(seller);
     }
 }
