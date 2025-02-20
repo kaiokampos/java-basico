b96273b59eec73ef3ef6fd878903dd035f2495b7
Kaio Kampos <kaiocamposti@gmail.com>
2025-02-20 13:29:50 -0300

    findById implementation

--- demo-dao-jdbc/src/main/java/application/Program.java
+++ demo-dao-jdbc/src/main/java/application/Program.java
@@ -9,11 +9,10 @@ import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
-        Department department = new Department(1, "books");
-
-        Seller seller = new Seller(1, "bob", "bob@gmail.com", new Date(), 3000.00, department);
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
+
+        Seller seller = sellerDao.findById(3);
 
         System.out.println(seller);
     }
--- demo-dao-jdbc/src/main/java/model/dao/DaoFactory.java
+++ demo-dao-jdbc/src/main/java/model/dao/DaoFactory.java
@@ -1,10 +1,11 @@
 package model.dao;
 
+import db.DB;
 import model.dao.implementacao.SellerDaoJDBC;
 
 public class DaoFactory {
 
     public static SellerDao createSellerDao(){
-        return new SellerDaoJDBC();
+        return new SellerDaoJDBC(DB.getConnection());
     }
 }
