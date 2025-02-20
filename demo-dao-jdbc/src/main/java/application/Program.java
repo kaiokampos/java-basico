53fbee5fb5cf569dd5c35b468b8ff776786ad633
Kaio Kampos <kaiocamposti@gmail.com>
2025-02-20 12:40:44 -0300

    SellerDaoJDBC and DaoFactory

--- demo-dao-jdbc/src/main/java/application/Program.java
+++ demo-dao-jdbc/src/main/java/application/Program.java
@@ -1,5 +1,7 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -10,6 +12,8 @@ public class Program {
         Department department = new Department(1, "books");
 
         Seller seller = new Seller(1, "bob", "bob@gmail.com", new Date(), 3000.00, department);
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
 
         System.out.println(seller);
     }
